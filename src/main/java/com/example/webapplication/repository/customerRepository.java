d21bd79e5c460a323e53ed5407bc6d136b73281a
Shubham dc <shubhamdc98@gmail.com>
2024-05-29 00:11:50 +0530

    lets

--- src/main/java/com/example/webapplication/repository/customerRepository.java
+++ src/main/java/com/example/webapplication/repository/customerRepository.java
@@ -2,7 +2,9 @@ package com.example.webapplication.repository;
 
 import com.example.webapplication.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface customerRepository extends JpaRepository<Customer, Long> {
 
 }
